d24f34cbfc3c2872b97cd4e8a277b866c4967b4f
licho08055 <iliyae547@gmail.com>
2022-07-24 21:46:57 +0100

    trial commit

--- planet_behave/settings.py
+++ planet_behave/settings.py
@@ -11,9 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import environ
-env = environ.Env()
-environ.Env.read_env()
+
+ 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,20 +76,13 @@ WSGI_APPLICATION = 'planet_behave.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-#DATABASES = {
-   # 'default': {
-   #     'ENGINE': 'django.db.backends.sqlite3',
-  #      'NAME': BASE_DIR / 'db.sqlite3',
-  #  }
-#}
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.postgresql_psycopg2',
-'NAME': env('DATABASE_NAME'),
-'USER': env('DATABASE_USER'),
-'PASSWORD': env('DATABASE_PASS'),
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-}
+ 
 
 
 
